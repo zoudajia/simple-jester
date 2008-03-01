8b0547ec23d21854056e6da69e5f1e3247d921a9
ivan_r_moore <ivan_r_moore@0e0c6b4d-b744-0410-8343-e8b2f34ae591>
2008-03-01 20:26:50 +0000

    Suppress serialization id warning for exceptions

--- trunk/simple-jester/src/main/java/jester/ConfigurationException.java
+++ trunk/simple-jester/src/main/java/jester/ConfigurationException.java
@@ -1,5 +1,6 @@
 package jester;
 
+@SuppressWarnings("serial")
 public class ConfigurationException extends SourceChangeException {
 
 	// TODO not logically a SourceChangeException - need to sort out exceptions
--- trunk/simple-jester/src/main/java/jester/JesterArgumentException.java
+++ trunk/simple-jester/src/main/java/jester/JesterArgumentException.java
@@ -1,5 +1,6 @@
 package jester;
 
+@SuppressWarnings("serial")
 public class JesterArgumentException extends Exception {
 
 	public JesterArgumentException(String msg) {
