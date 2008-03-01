5f1702843e15505fc38fc3ba6856e65904108785
ivan_r_moore <ivan_r_moore@0e0c6b4d-b744-0410-8343-e8b2f34ae591>
2008-03-01 13:43:28 +0000

    replacing a use of MockClassSourceChanger with jMock2

--- trunk/simple-jester/src/test/java/jester/tests/TwoStringSwappingCodeManglerTest.java
+++ trunk/simple-jester/src/test/java/jester/tests/TwoStringSwappingCodeManglerTest.java
@@ -2,12 +2,18 @@ package jester.tests;
 
 import jester.ClassSourceCodeChanger;
 import jester.CodeMangler;
+import jester.IgnoreList;
+import jester.IgnoreListDocument;
 import jester.SourceChangeException;
 import jester.TwoStringSwappingCodeMangler;
 import junit.framework.TestCase;
 
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+
 public class TwoStringSwappingCodeManglerTest extends TestCase {
 	private String originalString1 = "one", replacementString1 = "uno";
+	private Mockery context = new Mockery();
 
 	public TwoStringSwappingCodeManglerTest(String name) {
 		super(name);
@@ -18,21 +24,26 @@ public class TwoStringSwappingCodeManglerTest extends TestCase {
 	}
 
 	public void testCouldntMangle() throws SourceChangeException {
-		MockClassSourceChanger mockClassSourceChanger = new MockClassSourceChanger();
-		mockClassSourceChanger.setOriginalContents("abc def");
-		mockClassSourceChanger.setExpectedWriteOverSourceReplacingCalls(0);
+		final ClassSourceCodeChanger mockClassSourceChanger = context.mock(ClassSourceCodeChanger.class);
+		context.checking(new Expectations(){{
+			one(mockClassSourceChanger).getOriginalContents(); will(returnValue(new IgnoreListDocument("abc def", new IgnoreList(""))));
+			//is this how to do "never"?
+			never(mockClassSourceChanger).writeOverSourceReplacing(with(any(int.class)), with(any(String.class)), with(any(String.class)));
+		}});
 
 		CodeMangler codeMangler = newCodeMangler(mockClassSourceChanger);
 
 		assertTrue("couldn't make change - mock checks didnt try to make any change", !codeMangler.makeChangeToClass());
 
-		mockClassSourceChanger.verify();
+		context.assertIsSatisfied();
 	}
 
 	public void testSimpleMangle1() throws SourceChangeException {
-		MockClassSourceChanger mockClassSourceChanger = new MockClassSourceChanger();
-		mockClassSourceChanger.setOriginalContents("abc " + originalString1 + " def");
-		mockClassSourceChanger.setExpectedWriteOverSourceReplacing(4, originalString1, replacementString1);
+		final ClassSourceCodeChanger mockClassSourceChanger = context.mock(ClassSourceCodeChanger.class);
+		context.checking(new Expectations(){{
+			one(mockClassSourceChanger).getOriginalContents(); will(returnValue(new IgnoreListDocument("abc " + originalString1 + " def", new IgnoreList(""))));
+			one(mockClassSourceChanger).writeOverSourceReplacing(4, originalString1, replacementString1);
+		}});
 
 		CodeMangler codeMangler = newCodeMangler(mockClassSourceChanger);
 
