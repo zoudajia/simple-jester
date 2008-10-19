084ec103da02dedc17504f510cda09e9b9497df2
ivan_r_moore <ivan_r_moore@0e0c6b4d-b744-0410-8343-e8b2f34ae591>
2008-10-19 15:15:55 +0000

    updating for new executable jar way of running jester

--- src/main/resources/test.bat
+++ src/main/resources/test.bat
@@ -1,2 +1,2 @@
-java jester.TestTester "ant.bat -f test-build.xml" jester/acceptancetests
+java -jar simple-jester.jar "ant.bat -f test-build.xml" jester/acceptancetests
 python makeWebView.py -s -z
